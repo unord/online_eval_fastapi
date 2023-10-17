7288d22ae910e45f156b65ab76db608e7f63399b
Gorm Reventlow <gorm@reventlow.com>
2023-10-17 15:08:17 +0200

    updated selenium to 4.14.0

--- src/main.py
+++ src/main.py
@@ -11,12 +11,12 @@ class CloseEvalAndSendCsv(BaseModel):
     teacher_initials: str
 
 
-@app.get("/close_eval_and_send_csv/{username, password, refrence, teacher_initials}")
+@app.get("/close_eval_and_send_mail/{username, password, refrence, teacher_initials}")
 def close_eval_and_send_mail(username: str, password: str, refrence: str, teacher_initials: str) -> dict:
     tools.close_eval_and_send_mail(username, password, refrence, teacher_initials)
     return {'msg': 'success', 'success': True}
 
-@app.post("/close_eval_and_send_csv/")
+@app.post("/close_eval_and_send_mail/")
 def close_eval_and_send_mail(close_eval_and_send_csv: CloseEvalAndSendCsv) -> dict:
     msg = tools.close_eval_and_send_mail(close_eval_and_send_csv.username,close_eval_and_send_csv.password, close_eval_and_send_csv.refrence, close_eval_and_send_csv.teacher_initials)
     if msg['success']:
